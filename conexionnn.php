150c21a9ebf6fdb934d58cda48a436ec2286d216
ayayou18 <Shaib18@yahoo.fr>
2025-07-23 23:41:40 +0200

    Update conexionnn.php

--- conexionnn.php
+++ conexionnn.php
@@ -1,19 +1,26 @@
-   <?php
-   $host = "mysql-shaib18.alwaysdata.net"; // Reemplaza con tu host de Railway
-   $usuario = "shaib18"; // Reemplaza con tu usuario de Railway
-   $contrasena = "Amel-1126"; // Reemplaza con tu contraseña de Railway
-   $basededatos = "shaib18_testdb"; // Reemplaza con el nombre de tu base de datos de Railway
-   $puerto = 3306; // Puerto por defecto para MySQL
+<?php
+$servername = "mysql-shaib18.alwaysdata.net";
+$username = "shaib18";
+$password = "Amel-1126";
+$dbname = "shaib18_testdb";
+$puerto = 3306;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname,$puerto);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-   // Crear conexión
-   $conn = new mysqli($host, $usuario, $contrasena, $basededatos, $puerto);
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
 
-   // Verificar conexión
-   if ($conn->connect_error) {
-       die("Conexión fallida: " . $conn->connect_error);
-   }
-   echo "Conexión exitosa";
-
-   // Cierra la conexión cuando ya no sea necesaria
-   $conn->close();
-   ?>
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["edad"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
